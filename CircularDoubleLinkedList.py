df3dd62059e4121f08fa5e609baefdfc56f0d94b
xavicolet96 <fxavier.colet@gmail.com>
2015-04-21 14:25:58 +0200

    Creades diverses classes  i muntades algunes estructures

--- CircularDoubleLinkedList.py
+++ CircularDoubleLinkedList.py
@@ -1,1 +1,39 @@
 __author__ = 'xavier'
+
+class CircularDoubleLinkedList:
+    def __init__(self):
+        self.__head=None
+
+    def __len__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def isEmpty(self):
+        pass
+
+    def get(self,index):
+        pass
+
+    def __getitem__(self, item):
+        pass
+
+    def append(self,item):
+        pass
+
+    def insert(self,item,index):
+        pass
+
+    def remove(self, item):
+        pass
+
+    def __delitem__(self, index):
+        pass
+
+    def __iter__(self):
+        pass
+
+    @staticmethod
+    def test():
+        pass
---
+++ LinkedPriorityQueue.py
@@ -0,0 +1,10 @@
+__author__ = 'xavier'
+
+class LinkedPriorityQueue (LinquedQueue):
+
+
+    def __init__(self):
+        super(PriorityQueue, self).__init__()
+
+    def enqueue (self,item):
+        pass
